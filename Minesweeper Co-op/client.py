d7a1bc37f4280a1b6ed677c19fa896cbe60fe8af
svyatoslyav <swatock228@gmail.com>
2025-11-29 16:05:59 +0200

    server and client primary basic function

---
+++ Minesweeper Co-op/client.py
@@ -0,0 +1,26 @@
+import socket
+
+HEADER = 64
+SERVER = "26.8.108.156"
+PORT = 5055
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+
+MSG_DISCONNECT = "!DISCONNECT"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(message):
+    msg = message.encode(FORMAT)
+    msg_len = str(len(msg)).encode()
+    msg_len += b' ' * (HEADER - len(msg_len))
+    client.send(msg_len)
+    client.send(msg)
+
+while True:
+    msg = input()
+    if msg == MSG_DISCONNECT:
+        break
+    else:
+        send(msg)
